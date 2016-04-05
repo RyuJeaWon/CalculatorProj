1a30fac012b0fed7c8c74c84f2ac8360217221bb
user <user@user156>
2016-04-05 19:17:41 +0900

    complete Calculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -11,13 +11,19 @@ public class Calculator {
 	public int sub(int x,int y){
 		return x-y;
 	}
+	public int mul(int x,int y){
+		return x*y;
+	}
+	public double div(int x,int y){
+		return x/y;
+	}
 	public static void main(String[] args) {
-		
 		 Calculator cal = new  Calculator();
 
 		 System.out.println(cal.add(1, 2));
 		 System.out.println(cal.sub(2, 1));
-
+		 System.out.println(cal.mul(2, 2));
+		 System.out.println(cal.div(2, 2));
 	}
 
 }
